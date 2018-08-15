595afb5aebc8470d88e3116da6dd784d000de74e
lpinilla <lpinilla@itba.edu.ar>
2018-08-15 11:41:34 -0300

    empezado ejercicio 8

--- Guia2/ejer8.asm
+++ Guia2/ejer8.asm
@@ -7,18 +7,95 @@ _start:
 	pusha
 	mov eax, 0
 	mov ecx, 0
+	mov edx, 0
 	mov ebx, array
+							;A: contador de posiciones y longitud (maneja el ciclo externo)
+							;D: el valor más chico y su índice
 	mov ah, longitud
 
-	.loop:
-	cmp ah, cl
+	.loop:					;i
+	cmp ah, al
 	je .print
+	.findmin				;guarda el minimo en registro c
 
-	.findmin				;guarda el minimo en smallest
+	.findmin:				;k
+	mov cl, al 				;hay que arrancar desde el i-esimo numero
+
+	push ebx
+	mov ch, 0
+	add ebx, ecx 			;mover el puntero
+	mov dl, [ebx]			;guardar el i-esimo numero
+	pop ebx
+	inc cl
+
+	.minloop:				;recorrer desde i+1 hasta n los numeros
+	cmp cl, ah
+	je .swapmin				;acá el mínimo esta guardado en dl	
+	inc ebx
+	mov dh, [ebx]			;cargamos el k-esimo valor, estoy pisando el índice del mas chico
+	cmp dl, dh
+	ja .swap	
+	inc cl
+	jmp .minloop
+
+	.swap:
+	mov dl, dh				;agarramos el valor más chico
+	mov ch, cl 				;agarramos su índice
+	inc cl
+	jmp .minloop
+
+	.swapmin:
+	mov ebx, array 			;recuperamos el puntero a la posicion original
+
+	push eax				;guardamos en dh el valor array[al]
+	mov ah, 0
+	add ebx, eax
+	mov dh, [ebx]
+	pop eax
+
+	mov ebx, array 			;guardmaos el valor dh en array[ch]
+	push ecx
+	mov cl, ch
+	mov ch, 0
+	add ebx, ecx
+	mov [ebx], dh
+	pop ecx
+
+	mov ebx, array 			;guardamos el valor dl en array[al]
+	push eax
+	mov ah, 0
+	add ebx, eax
+	mov [ebx], dl
+	pop eax
+
+	mov ebx, array
+	inc al
+	jmp .loop
 
 
+	.print:					
+	mov al, 0
 
+	.printloop:
+	cmp ah, al
+	je .end
 
+	push ebx
+	push eax
+
+	mov ah, 0
+	add ebx, eax
+	mov al, [ebx]
+	mov ebx, smallest
+	call ejer3
+	call print
+
+	pop eax
+	pop ebx
+	inc al
+	jmp .printloop
+
+	.end:
 	popa
 	popf
 
